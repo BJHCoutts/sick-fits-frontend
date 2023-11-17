53efa2c783bf6aafb60883726ac65159d15b10fa
Brian Coutts <bjhcoutts@gmail.com>
2023-11-16 17:54:42 -0800

    file structure, basic nav, & autoformatting added

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,16 @@
+import Link from 'next/link'
+import Nav from './Nav'
+
 export default function Header() {
-	return(
+	return (
 		<header>
 			<h2>Header</h2>
+			<div className="bar">
+				<Link href={'/'}>Sick Fits</Link>
+			</div>
+			<div className="sub-bar">
+				<Nav />
+			</div>
 		</header>
 	)
 }
--- src/pages/orders/index.tsx
+++ src/pages/orders/index.tsx
@@ -1,3 +1,3 @@
 export default function OrderIndexPage() {
-  return <h1>Order</h1>
+	return <h1>Orders</h1>
 }
