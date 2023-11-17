52fe9b212405459dd2a5ee0eee1d78b8f21d7ac5
Brian Coutts <bjhcoutts@gmail.com>
2023-11-16 18:28:47 -0800

    files converted to TS, styled components added

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,16 +1,51 @@
 import Link from 'next/link'
 import Nav from './Nav'
+import styled from 'styled-components'
+
+const Logo = styled.h1`
+	font-size: 4rem;
+	margin-left: 2em;
+	position: relative;
+	z-index: 2;
+	background-color: red;
+	transform: skewX(-7deg);
+
+	a {
+		color: white;
+		text-decoration: none;
+		text-transform: uppercase;
+		padding: 0.5em 1em;
+	}
+`
+
+const HeaderStyled = styled.header`
+	.bar {
+		border-bottom: 10px solid var(--black, black);
+		display: grid;
+		grid-template-columns: auto 1fr;
+		justify-content: space-between;
+		align-items: center;
+	}
+
+	.sub-bar {
+		display: grid;
+		grid-template-columns: 1fr auto;
+		border-bottom: 1px solid var(--black, black);
+	}
+`
 
 export default function Header() {
 	return (
-		<header>
+		<HeaderStyled>
 			<h2>Header</h2>
 			<div className="bar">
-				<Link href={'/'}>Sick Fits</Link>
+				<Logo>
+					<Link href={'/'}>Sick Fits</Link>
+				</Logo>
 			</div>
 			<div className="sub-bar">
 				<Nav />
 			</div>
-		</header>
+		</HeaderStyled>
 	)
 }
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,15 +1,16 @@
-import Footer from "../components/Footer"
-import Header from "../components/Header"
+import Header from '../components/Header'
+import Footer from '../components/Footer'
+
 import { AppProps } from 'next/app'
 
-export default function myApp({Component, pageProps}: AppProps) {
-	return(<>
-
-		<Header/>
-		<main>
-			<Component {...pageProps} />
-		</main>
-		<Footer/>
-
-	</>)
+export default function myApp({ Component, pageProps }: AppProps) {
+	return (
+		<>
+			<Header />
+			<main>
+				<Component {...pageProps} />
+			</main>
+			<Footer />
+		</>
+	)
 }
