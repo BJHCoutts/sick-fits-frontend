d7eaafcd2228a654d25e9453e90f9e54083edadb
Brian Coutts <bjhcoutts@gmail.com>
2023-11-16 18:35:39 -0800

    global styles setup started

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,17 @@
 import Header from '../components/Header'
 import Footer from '../components/Footer'
 
+import { createGlobalStyle } from 'styled-components'
 import { AppProps } from 'next/app'
+
+const GlobalStyles = createGlobalStyle`
+	
+`
 
 export default function myApp({ Component, pageProps }: AppProps) {
 	return (
 		<>
+			<GlobalStyles />
 			<Header />
 			<main>
 				<Component {...pageProps} />
