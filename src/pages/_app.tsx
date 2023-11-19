e7d656ea4b01d1b9faeaca0deaf670a90c168789
Brian Coutts <bjhcoutts@gmail.com>
2023-11-19 13:23:28 -0800

    global styles in, brand font added

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,21 +1,24 @@
 import Header from '../components/Header'
 import Footer from '../components/Footer'
 
-import { createGlobalStyle } from 'styled-components'
 import { AppProps } from 'next/app'
+import { GlobalStyle } from '../components/styles/GlobalStyle'
+import styled from 'styled-components'
 
-const GlobalStyles = createGlobalStyle`
-	
+const InnerStyles = styled.main`
+	max-width: var(--maxWidth);
+	margin: 0 auto;
+	padding: 2rem;
 `
 
 export default function myApp({ Component, pageProps }: AppProps) {
 	return (
 		<>
-			<GlobalStyles />
+			<GlobalStyle />
 			<Header />
-			<main>
+			<InnerStyles>
 				<Component {...pageProps} />
-			</main>
+			</InnerStyles>
 			<Footer />
 		</>
 	)
