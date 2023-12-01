bbcd684db6c1dc213b07dd3ae7f8f9562505c4a1
Brian Coutts <bjhcoutts@gmail.com>
2023-11-30 22:52:26 -0800

    apollo client has added, getInitialProps fix

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -8,20 +8,28 @@ import Router from 'next/router'
 import { AppProps } from 'next/app'
 import { GlobalStyle } from '../components/styles/GlobalStyle'
 import styled from 'styled-components'
+import { ApolloProvider } from '@apollo/client'
+import withData from '../lib/withData'
 
 Router.events.on('routeChangeStart', () => NProgress.start())
 Router.events.on('routeChangeComplete', () => NProgress.done())
 Router.events.on('routeChangeError', () => NProgress.done())
 
-export default function myApp({ Component, pageProps }: AppProps) {
+interface IMyApp extends AppProps {
+	apollo: any
+}
+
+function myApp({ Component, pageProps, apollo }: IMyApp) {
 	return (
 		<>
 			<GlobalStyle />
-			<Header />
-			<InnerStyles>
-				<Component {...pageProps} />
-			</InnerStyles>
-			<Footer />
+			<ApolloProvider client={apollo}>
+				<Header />
+				<InnerStyles>
+					<Component {...pageProps} />
+				</InnerStyles>
+				<Footer />
+			</ApolloProvider>
 		</>
 	)
 }
@@ -31,3 +39,4 @@ const InnerStyles = styled.main`
 	margin: 0 auto;
 	padding: 2rem;
 `
+export default withData(myApp)
