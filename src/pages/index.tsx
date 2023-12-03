0ecdb373038935241e198de39782f6d3af1b8b3b
Brian Coutts <bjhcoutts@gmail.com>
2023-12-03 14:38:51 -0800

    basic test metadata put into home index page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,6 +5,10 @@ export default function HomeIndexPage() {
 		<>
 			<Head>
 				<title>Sick Fits!</title>
+				<meta name="description" content="Sick Fits Description" />
+				<meta name="keywords" content="Sick Fits Keywords" />
+				<meta name="author" content="Brian Coutts" />
+				<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 			</Head>
 			<h1>Home</h1>
 		</>
