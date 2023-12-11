7a7ca983985e74dd9417d38e509d693cd0484674
Brian Coutts <bjhcoutts@gmail.com>
2023-12-10 18:12:54 -0800

    create product gql mutation added

---
+++ src/lib/graphQL/mutations/createProductMutation.ts
@@ -0,0 +1,28 @@
+import { gql } from "@apollo/client"
+
+export const CREATE_PRODUCT_MUTATION = gql`
+	mutation CREATE_PRODUCT_MUTATION(
+		$name: String!
+		$description: String!
+		$price: Int!
+		$image: Upload
+	){
+		createProduct (data:{
+			name:$name
+			description:$description
+			price: $price
+			status: "AVAILABLE"
+			photo: {
+				create: {
+					image: $image,
+					altText: $name
+				}
+			}
+		}) {
+			id
+			name
+			description
+			price
+		}
+	}
+`
