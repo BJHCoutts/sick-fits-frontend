c750aa674623e376f29e9e52b31380d31f46f37c
Brian Coutts <bjhcoutts@gmail.com>
2023-12-24 11:45:40 -0800

    query user hook, sign in page start

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,22 +1,35 @@
 import Link from 'next/link'
 import SNav from './styles/SNav'
+import { useUser } from '../lib/functions/useUser'
 
 export default function Nav() {
+	const user = useUser()
+	console.log(user)
 	return (
 		<SNav>
 			<ul>
 				<li>
 					<Link href="/products">Products</Link>
 				</li>
-				<li>
-					<Link href="/sell">Sell</Link>
-				</li>
-				<li>
-					<Link href="/orders">Orders</Link>
-				</li>
-				<li>
-					<Link href="/account">Account</Link>
-				</li>
+				{user ? (
+					<>
+						<li>
+							<Link href="/sell">Sell</Link>
+						</li>
+						<li>
+							<Link href="/orders">Orders</Link>
+						</li>
+						<li>
+							<Link href="/account">Account</Link>
+						</li>
+					</>
+				) : (
+					<>
+						<li>
+							<Link href="/sign-in">Sign In</Link>
+						</li>
+					</>
+				)}
 			</ul>
 		</SNav>
 	)
---
+++ src/pages/signin/index.tsx
@@ -0,0 +1,12 @@
+import Head from 'next/head'
+
+export default function SignIn() {
+	return (
+		<>
+			<Head>
+				<title>Sign In Page</title>
+			</Head>
+			<h1>Sign In</h1>
+		</>
+	)
+}
