88e9e8e0c5d5fe4a25229fcf7633f40eef10d6fc
Brian Coutts <bjhcoutts@gmail.com>
2024-01-01 20:07:28 -0800

    user query now queries cart

--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -1,5 +1,32 @@
+import styled from 'styled-components'
+import { useUser } from '../lib/functions/useUser'
 import SCart from './styles/SCart'
+import Supreme from './styles/Supreme'
 
 export default function Cart() {
-	return <SCart open>Oh Hi</SCart>
+	const user = useUser()
+	if (!user) return null
+	return (
+		<SCart open>
+			<header>
+				<Supreme>{user.name}'s Cart</Supreme>
+			</header>
+			{user?.cart && (
+				<ul>
+					{user.cart.map((cartItem) => (
+						<CartItem key={cartItem.id} cartItem={cartItem} />
+					))}
+				</ul>
+			)}
+		</SCart>
+	)
 }
+
+function CartItem({ cartItem }) {
+	return <SCartItem>{cartItem.id}</SCartItem>
+}
+
+const SCartItem = styled.li`
+	padding: 1rem 0;
+	border-bottom: 1px solid var(--lightGrey);
+`
--- src/lib/graphQL/queries/currentUserQuery.ts
+++ src/lib/graphQL/queries/currentUserQuery.ts
@@ -7,7 +7,23 @@ export const CURRENT_USER_QUERY = gql`
         id
         email
         name
-        # TODO: Query the cart once we have it
+        cart {
+          id
+          quantity
+          product {
+            id
+            name
+            description
+            price
+            photo {
+              id
+              altText
+              image {
+                publicUrlTransformed
+              }
+            }
+          }
+        }
       }
     }
   }
