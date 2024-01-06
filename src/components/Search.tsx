2890b2fbecbd0747d61d3d20db0250908077ca3f
Brian Coutts <bjhcoutts@gmail.com>
2024-01-06 13:48:22 -0800

    search dropdown and pge routing complete

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -3,8 +3,11 @@ import { SDropDown, SDropDownItem, SSearch } from './styles/SDropDown'
 import { SEARCH_ALL_PRODUCTS_QUERY } from '../lib/graphQL/queries/searchAllProductsQuery'
 import { useLazyQuery } from '@apollo/client'
 import { debounce } from '../lib/functions/debounce'
+import { TProduct } from '../lib/types/TProduct'
+import { useRouter } from 'next/router'
 
 export default function Search() {
+	const router = useRouter()
 	const [findItems, { loading, data, error }] = useLazyQuery(
 		SEARCH_ALL_PRODUCTS_QUERY,
 		{
@@ -12,21 +15,24 @@ export default function Search() {
 		}
 	)
 
-	const { getInputProps, getItemProps } = useCombobox({
-		items: [],
-		onInputValueChange({ inputValue }) {
-			console.log('input changed to ' + inputValue)
-			debounce(getData)
-			function getData() {
-				console.log(inputValue)
-			}
-		},
-		onSelectedItemChange() {
-			console.log('selected item changed')
-		},
-	})
+	const items = data?.searchTerms || []
 
-	function handleChange() {}
+	const {
+		getInputProps,
+		getItemProps,
+		highlightedIndex,
+		isOpen,
+		inputValue: globalInputValue,
+	} = useCombobox({
+		items,
+		onInputValueChange({ inputValue }) {
+			debounce(() => findItems({ variables: { searchTerm: inputValue } }), 350)
+		},
+		onSelectedItemChange({ selectedItem }: { selectedItem: TProduct }) {
+			router.push({ pathname: `/product/${selectedItem.id}` })
+		},
+		itemToString: (item: TProduct) => item?.name || '',
+	})
 
 	return (
 		<SSearch>
@@ -35,13 +41,28 @@ export default function Search() {
 					type: 'search',
 					placeholder: 'Search for an item',
 					id: 'search',
-					className: 'loading',
+					className: loading ? 'loading' : '',
 				})}
 			/>
 			<SDropDown>
-				<SDropDownItem {...getItemProps}>1</SDropDownItem>
-				<SDropDownItem {...getItemProps}>2</SDropDownItem>
-				<SDropDownItem {...getItemProps}>3</SDropDownItem>
+				{isOpen &&
+					items.map((item: TProduct, index) => (
+						<SDropDownItem
+							key={item.id}
+							{...getItemProps({ item })}
+							highlighted={index === highlightedIndex}
+						>
+							<img
+								src={item.photo.image.publicUrlTransformed}
+								alt={item.name}
+								width="50"
+							/>
+							{item.name}
+						</SDropDownItem>
+					))}
+				{isOpen && !items.length && !loading && globalInputValue.length && (
+					<SDropDownItem>Sorry, no items for {globalInputValue}</SDropDownItem>
+				)}
 			</SDropDown>
 		</SSearch>
 	)
