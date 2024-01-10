c1ab9a08e89a10e106b40623724909e99b63aa33
Brian Coutts <bjhcoutts@gmail.com>
2024-01-10 13:18:13 -0800

    orderS page done

--- src/pages/orders/index.tsx
+++ src/pages/orders/index.tsx
@@ -5,6 +5,13 @@ import SOrder from '../../components/styles/SOrder'
 import { TOrder } from '../../lib/types/TOrder'
 import formatMoney from '../../lib/functions/formatMoney'
 import { USER_ORDERS_QUERY } from '../../lib/graphQL/queries/userOrdersQuery'
+import styled from 'styled-components'
+import SOrderItem from '../../components/styles/SOrderItem'
+import Link from 'next/link'
+
+function countItemsPerOrder(order: TOrder) {
+	return order.items.reduce((tally, item) => tally + item.quantity, 0)
+}
 
 export default function OrdersPage() {
 	const { data, error, loading } = useQuery(USER_ORDERS_QUERY)
@@ -16,9 +23,41 @@ export default function OrdersPage() {
 	return (
 		<>
 			<Head>
-				<title>Sick Fits | Orders</title>
+				<title>{`Your Orders ${allOrders.length}`}</title>
 			</Head>
-			<div>{allOrders.length}</div>
+			<h2>You Have {allOrders.length} Orders</h2>
+			<SOrderUl>
+				{allOrders.map((order) => {
+					return (
+						<SOrderItem>
+							<Link href={`/order/${order.id}`}>
+								<div className="order-meta">
+									<p>
+										{countItemsPerOrder(order)} Product
+										{order.items.length > 1 ? 's' : ''}
+									</p>
+									<div className="images">
+										{order.items.map((item) => (
+											<img
+												key={`image-${item.id}`}
+												src={item.photo?.image?.publicUrlTransformed}
+												alt={item.name}
+											/>
+										))}
+									</div>
+									<p>{formatMoney(order.total)}</p>
+								</div>
+							</Link>
+						</SOrderItem>
+					)
+				})}
+			</SOrderUl>
 		</>
 	)
 }
+
+const SOrderUl = styled.div`
+	display: grid;
+	grid-template-columns: repeat(auto-fit, minmax(350px, 1fr));
+	grid-gap: 4rem;
+`
