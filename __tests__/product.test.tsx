756d1e9851895a43b3df0d6c99d9ed103d38e238
Brian Coutts <bjhcoutts@gmail.com>
2024-01-25 20:10:56 -0800

    jest and react testing library TS all setup and working

---
+++ __tests__/product.test.tsx
@@ -0,0 +1,7 @@
+import { render } from '@testing-library/react'
+
+describe('JSX tags', () => {
+	it('renders a div', () => {
+		render(<p>Hello World</p>)
+	})
+})
--- src/lib/testUtils.ts
+++ src/lib/testUtils.ts
@@ -40,99 +40,99 @@ const fakeItem = ():TProduct => ({
   description: 'dogs',
 } );
 
-const fakeUser = (overrides) => ({
-  __typename: 'User',
-  id: '4234',
-  name: casual.name,
-  email: casual.email,
-  permissions: ['ADMIN'],
-  orders: [],
-  cart: [],
-  ...overrides,
-});
+// const fakeUser = ( overrides: { overrides:unknown } ) => ({
+//   __typename: 'User',
+//   id: '4234',
+//   name: casual.name,
+//   email: casual.email,
+//   permissions: ['ADMIN'],
+//   orders: [],
+//   cart: [],
+//   ...overrides,
+// });
 
-const fakeOrderItem = () => ({
-  __typename: 'OrderItem',
-  id: casual.uuid,
-  image: {
-    image: `${casual.word}.jpg`,
-  },
-  name: casual.words(),
-  price: 4234,
-  quantity: 1,
-  description: casual.words(),
-});
+// const fakeOrderItem = () => ({
+//   __typename: 'OrderItem',
+//   id: casual.uuid,
+//   image: {
+//     image: `${casual.word}.jpg`,
+//   },
+//   name: casual.words(),
+//   price: 4234,
+//   quantity: 1,
+//   description: casual.words(),
+// });
 
-const fakeOrder = () => ({
-  __typename: 'Order',
-  id: 'ord123',
-  charge: 'ch_123',
-  total: 40000,
-  items: [fakeOrderItem(), fakeOrderItem()],
-  createdAt: '2022-12-11T20:16:13.797Z',
-  user: fakeUser(),
-});
+// const fakeOrder = () => ({
+//   __typename: 'Order',
+//   id: 'ord123',
+//   charge: 'ch_123',
+//   total: 40000,
+//   items: [fakeOrderItem(), fakeOrderItem()],
+//   createdAt: '2022-12-11T20:16:13.797Z',
+//   user: fakeUser(),
+// });
 
-const fakeCartItem = (overrides) => ({
-  __typename: 'CartItem',
-  id: 'omg123',
-  quantity: 3,
-  product: fakeItem(),
-  user: fakeUser(),
-  ...overrides,
-});
+// const fakeCartItem = (overrides) => ({
+//   __typename: 'CartItem',
+//   id: 'omg123',
+//   quantity: 3,
+//   product: fakeItem(),
+//   user: fakeUser(),
+//   ...overrides,
+// });
 
-// Fake LocalStorage
-class LocalStorageMock {
-  constructor() {
-    this.store = {};
-  }
+// // Fake LocalStorage
+// class LocalStorageMock {
+//   constructor() {
+//     this.store = {};
+//   }
 
-  clear() {
-    this.store = {};
-  }
+//   clear() {
+//     this.store = {};
+//   }
 
-  getItem(key) {
-    return this.store[key] || null;
-  }
+//   getItem(key) {
+//     return this.store[key] || null;
+//   }
 
-  setItem(key, value) {
-    this.store[key] = value.toString();
-  }
+//   setItem(key, value) {
+//     this.store[key] = value.toString();
+//   }
 
-  removeItem(key) {
-    delete this.store[key];
-  }
-}
+//   removeItem(key) {
+//     delete this.store[key];
+//   }
+// }
 
-function makePaginationMocksFor(length) {
-  return [
-    {
-      request: { query: PAGINATION_QUERY },
-      result: {
-        data: {
-          _allProductsMeta: {
-            count: length,
-          },
-          itemsConnection: {
-            __typename: 'aggregate',
-            aggregate: {
-              count: length,
-              __typename: 'count',
-            },
-          },
-        },
-      },
-    },
-  ];
-}
+// function makePaginationMocksFor(length:number) {
+//   return [
+//     {
+//       request: { query: PAGINATION_QUERY },
+//       result: {
+//         data: {
+//           _allProductsMeta: {
+//             count: length,
+//           },
+//           itemsConnection: {
+//             __typename: 'aggregate',
+//             aggregate: {
+//               count: length,
+//               __typename: 'count',
+//             },
+//           },
+//         },
+//       },
+//     },
+//   ];
+// }
 
 export {
-  makePaginationMocksFor,
-  LocalStorageMock,
+  // makePaginationMocksFor,
+  // LocalStorageMock,
   fakeItem,
-  fakeUser,
-  fakeCartItem,
-  fakeOrder,
-  fakeOrderItem,
+  // fakeUser,
+  // fakeCartItem,
+  // fakeOrder,
+  // fakeOrderItem,
 };
