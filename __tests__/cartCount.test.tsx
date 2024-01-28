64b5940f5b0fd75dd7ce70e248968aad9aeac43d
Brian Coutts <bjhcoutts@gmail.com>
2024-01-27 17:25:18 -0800

    async cart count tests added

--- __tests__/cartCount.test.tsx
+++ __tests__/cartCount.test.tsx
@@ -1,4 +1,4 @@
-import { render } from '@testing-library/react'
+import { render, screen, waitFor } from '@testing-library/react'
 import CartCount from '../src/components/CartCount'
 
 describe('<CartCount />', () => {
@@ -9,9 +9,15 @@ describe('<CartCount />', () => {
 		const { container } = render(<CartCount count={12} />)
 		expect(container).toMatchSnapshot()
 	})
-	it('updates via props', () => {
-		const { container } = render(<CartCount count={12} />)
+	it('updates via props', async () => {
+		const { container, rerender, debug } = render(<CartCount count={12} />)
 		expect(container.textContent).toBe('12')
 		expect(container).toHaveTextContent('12')
+		rerender(<CartCount count={13} />)
+		await screen.findByText('13')
+		await waitFor(() => {
+			expect(container.textContent).toBe('13')
+		})
+		debug()
 	})
 })
